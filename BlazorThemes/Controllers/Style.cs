b1087db7e97999377737792b01c9275261704ffe
Zoltan Halasz <zoltan.halasz@outlook.com>
2021-02-26 17:13:30 +0200

    Add project files.

---
+++ BlazorThemes/Controllers/Style.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorThemes.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class Style : ControllerBase
+    {
+        public async Task<ActionResult> ChangeStyle([FromQuery] string style)
+        {
+            Response.Cookies.Append("style", style);
+            return Redirect("/");
+        }
+    }
+}
